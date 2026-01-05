d2ad581d1876f854283d381b3b135fe3e03d2111
wangruijun <51185779+qsswgl@users.noreply.github.com>
2026-01-05 08:03:10 +0800

    Add src/main/java/com/example/pwawebview/MainActivity.java

---
+++ src/main/java/com/example/pwawebview/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.pwawebview;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://www.qsgl.net/html/365/pwa/#/");
+    }
+}
+
+gradle wrapper
